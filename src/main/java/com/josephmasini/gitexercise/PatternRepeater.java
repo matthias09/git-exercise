bf6dd374a75734dd08f6f17127a47c25a69875db
Joseph Masini <joseph.masini1@gmail.com>
2020-02-20 21:13:50 +0100

    Example Commit

---
+++ src/main/java/com/josephmasini/gitexercise/PatternRepeater.java
@@ -0,0 +1,9 @@
+package com.josephmasini.gitexercise;
+
+public class PatternRepeater {
+
+    /* Implement method to sort an array of integers */
+
+    /* Implement method to sort an array of strings */
+
+}
---
+++ src/test/java/com/josephmasini/gitexercise/PatternRepeaterTest.java
@@ -0,0 +1,7 @@
+package com.josephmasini.gitexercise;
+
+public class PatternRepeaterTest {
+
+    /* Implement tests */
+
+}
